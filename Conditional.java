f276389de1236004edc07443b50697c423a2ef98
VBonamassa3 <157747000+VBonamassa3@users.noreply.github.com>
2024-01-30 10:58:04 -0500

    conditional commit

---
+++ Conditional.java
@@ -0,0 +1,30 @@
+public class Conditional {
+    public static void main(String args[]){
+        //if statement
+        int x = 10;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+        }
+
+        //if else statement
+        int num = 7;
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+        //nested if else statement
+        int age = 18;
+        if (age >= 18) {
+            if (age < 21) {
+                System.out.println("You're eligible but not for alcohol");
+            } else {
+                System.out.println("You're eligible for everything");
+            }
+        } else {
+            System.out.println("You're not eligible");
+        }
+
+    }
+}
